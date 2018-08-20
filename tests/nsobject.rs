dc409e6706876f7985359d1f34db533486bdae6a
Michael Wu <mwu.code@gmail.com>
2018-08-19 22:43:02 -0400

    Test class methods

--- tests/nsobject.rs
+++ tests/nsobject.rs
@@ -10,3 +10,17 @@ fn nsobject_new() {
     let obj = obj.unwrap();
     assert_eq!(obj.isProxy(), false);
 }
+
+#[test]
+fn nsobject_description() {
+    let desc = NSObject::description();
+    assert_eq!(desc.is_some(), true);
+
+    let desc = desc.unwrap();
+    let desclen = desc.length();
+    let ruststr: String =
+        (0..desclen).map(|i|
+                         std::char::from_u32(desc.characterAtIndex_(i) as u32).
+                         unwrap()).collect();
+    assert_eq!(&ruststr, "NSObject");
+}
