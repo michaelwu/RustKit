bcf38023c47f444dc664fa8eb1410dee92eabf34
Michael Wu <mwu.code@gmail.com>
2018-08-23 21:07:37 -0400

    Rename rust_gen to rustkit_bindgen

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern crate rust_gen as gen;
+extern crate rustkit_bindgen as gen;
 
 use std::env;
 use std::path::Path;
